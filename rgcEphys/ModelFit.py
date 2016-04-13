f8b54a03e6de93a180024342843f72783bb979ff
tstadler <ths.stadler@gmail.com>
2016-04-13 13:21:36 +0200

    added ridge regression rf fit

--- rgcEphys/ModelFit.py
+++ rgcEphys/ModelFit.py
@@ -7,15 +7,23 @@ from sklearn.cross_validation import KFold
 
 from rgcEphys import RgcEphys
 
-
 class lnp_fit:
 
 
-    def stim_conv(filename, ch_trigger, ch_voltage, rec_type, mseq, freq=5, deltat=1000, fs=10000):
+    def stim_conv(filename, ch_voltage, ch_trigger, rec_type, mseq, freq=5, deltat=1000, fs=10000):
 
         """
         Convolves the stimulus with the time-kernel obtained from sta and returns the convolved stimulus
         together with the instantaneous spike-triggered average
+
+        :param filename: str '/path/to/example.h5'
+        :param ch_voltage: str 'name' of the recording channel containing a voltage signal recorded in gap-free mode
+        :param ch_trigger: str 'name' of the recording channel containing a trigger signal recorded in gap-free mode
+        :param rec_type: enum('intracell', 'extracell') patch mode
+        :param mseq: str '/path/to/mseq'
+        :param freq scalar stimulation frequency in Hz
+        :param deltat scalar time lag in s for calculating the time kernel
+        :param fs: scalar sampling rate in Hz
 
         """
 
@@ -101,15 +109,15 @@ class lnp_fit:
 
             return F_conv, sta_inst
 
-    def lnp_exp(self,filename, ch_trigger, ch_voltage, rec_type, mseq, fun, jac, w0, k, freq=5, fs=10000):
+    def lnp(self,filename, ch_trigger, ch_voltage, rec_type, mseq, ll_fun, jac, w0, k, freq=5, fs=10000):
 
         """
-        Fit the instantaneous RF of an LNP model with exponential non-linearity
+        Fit the instantaneous RF of an LNP model with the given likelihood fun
 
         :param s_conv array (stimDim[0]*stimDim[1],T) stimulus projected onto time kernel
         :param spiketimes array (1,nSpikes)
         :param triggertimes array(1,T)
-        :param fun function pointer to the negative log-likelihood function
+        :param ll_fun function pointer to the negative log-likelihood function
         :param jac boolen indicating whether fun returns gradient as well, grad has dimension (n,1)
         :param w0 array (n,1) initial rf guess
         :param k scalar k-fold cross-validation performed on the data set
@@ -132,20 +140,21 @@ class lnp_fit:
         y = np.histogram(spiketimes, bins=T,
                          range=[triggertimes[0], triggertimes[len(triggertimes) - 1] + (fs / freq)])[0]
 
-
-        kf = KFold(T, n_folds=k)
         LNP_dict = {}
         LNP_dict.clear()
-        LNP_dict['nLL'] = []
+        LNP_dict['nLL train'] = []
+        LNP_dict['nLL test'] = []
         LNP_dict['w'] = []
         LNP_dict['pred perform'] = []
         LNP_dict['r2'] = []
 
+        kf = KFold(T, n_folds=k)
         for train, test in kf:
-            res = scoptimize.minimize(fun, w0, args=(s[:, train], y[train]), jac=jac)
+            res = scoptimize.minimize(ll_fun, w0, args=(s[:, train], y[train]), jac=jac, method='TNC')
             print(res.message, 'neg log-liklhd: ', res.fun)
 
-            LNP_dict['nLL'].append(res.fun)
+            LNP_dict['nLL train'].append(res.fun)
+            LNP_dict['nLL test'].append(ll_fun(res.x, s[:, test], y[test])[0])
             LNP_dict['w'].append(res.x)
 
             y_test = np.zeros(len(test))
@@ -159,3 +168,217 @@ class lnp_fit:
 
         return LNP_df
 
+    def lnp_exp(self, filename, ch_trigger, ch_voltage, rec_type, mseq, k, freq=5, fs=10000):
+
+        """
+        Fit the instantaneous RF of an LNP model with exponential non-linearity
+
+        :param s_conv array (stimDim[0]*stimDim[1],T) stimulus projected onto time kernel
+        :param spiketimes array (1,nSpikes)
+        :param triggertimes array(1,T)
+        :param k scalar k-fold cross-validation performed on the data set
+
+        """
+        (voltage_trace, rec_len, spiketimes) = RgcEphys.preproc.spike_detect(filename, rec_type, ch_voltage)
+
+        (trigger_trace, triggertimes) = RgcEphys.preproc.trigger_detect(filename, ch_trigger)
+
+        s_conv, sta_inst = self.stim_conv(filename, ch_trigger, ch_voltage, rec_type, mseq)
+
+        s = np.transpose(s_conv)  # make it a (n x T) array
+
+        spiketimes = spiketimes[spiketimes > triggertimes[0]]
+        spiketimes = spiketimes[spiketimes < triggertimes[len(triggertimes) - 1] + (fs / freq)]
+
+        # define log-likelihood for lnp_exp
+
+        def nLL(wT, s, y):
+            """
+            Compute the negative log-likelihood of an LNP model wih exponential non-linearity
+
+            :param wT: current receptive field array(stimDim[0]*stimDim[1],)
+            :param s: stimulus array(stimDim[0]*stimDim[1],T)
+            :param y: spiketimes array(,T)
+
+            :return nLL: computed negative log-likelihood scalar
+            :return dnLL: computed first derivative of the nLL
+            """
+
+            r = np.exp(np.dot(wT, s))
+            nLL = np.dot(r - y * np.log(r), np.ones(y.shape))
+
+            dnLL = np.dot(s * r - y * s, np.ones(y.shape))
+
+            return nLL, dnLL
+
+        # bin spiketimes as stimulus frames
+        T = s.shape[1]
+
+        y = np.histogram(spiketimes, bins=T,
+                         range=[triggertimes[0], triggertimes[len(triggertimes) - 1] + (fs / freq)])[0]
+        w0 = np.zeros(sta_inst.shape)
+
+        kf = KFold(T, n_folds=k)
+        LNP_dict = {}
+        LNP_dict.clear()
+        LNP_dict['nLL train'] = []
+        LNP_dict['nLL test'] = []
+        LNP_dict['w'] = []
+        LNP_dict['pred perform'] = []
+        LNP_dict['r2'] = []
+
+        for train, test in kf:
+            res = scoptimize.minimize(nLL, w0, args=(s[:, train], y[train]), jac=True, method='TNC')
+            print(res.message, 'neg log-liklhd: ', res.fun)
+
+            LNP_dict['nLL train'].append(res.fun)
+            LNP_dict['nLL test'].append(nLL(res.x, s[:, test], y[test])[0])
+            LNP_dict['w'].append(res.x)
+
+            y_test = np.zeros(len(test))
+            for t in range(len(test)):
+                r = np.exp(np.dot(res.x, s[:, test[t]]))
+                y_test[t] = np.random.poisson(lam=r)
+
+            LNP_dict['pred perform'].append((sum(y_test == y[test]) / len(test)))
+            LNP_dict['r2'].append(np.var(y[test]) / np.var(y_test))
+        LNP_df = pd.DataFrame(LNP_dict)
+
+        return LNP_df
+
+    def lnp_exp_ridge(self, filename, ch_trigger, ch_voltage, rec_type, mseq, k, theta, freq=5, fs=10000):
+
+        """
+        Fit the instantaneous RF of an LNP model with exponential non-linearity nad ridge regression on the filter components
+
+        :param s_conv array (stimDim[0]*stimDim[1],T) stimulus projected onto time kernel
+        :param spiketimes array (1,nSpikes)
+        :param triggertimes array(1,T)
+        :param k scalar k-fold cross-validation performed on the data set
+        :param theta list (1 x nTheta) with values that should be cross-validated for the regression parameter
+
+        """
+        (voltage_trace, rec_len, spiketimes) = RgcEphys.preproc.spike_detect(filename, rec_type, ch_voltage)
+
+        (trigger_trace, triggertimes) = RgcEphys.preproc.trigger_detect(filename, ch_trigger)
+
+        s_conv, sta_inst = self.stim_conv(filename, ch_trigger, ch_voltage, rec_type, mseq)
+
+        s = np.transpose(s_conv)  # make it a (n x T) array
+
+        spiketimes = spiketimes[spiketimes > triggertimes[0]]
+        spiketimes = spiketimes[spiketimes < triggertimes[len(triggertimes) - 1] + (fs / freq)]
+
+        # define log-likelihood for lnp_exp
+
+        def nLL(wT, s, y):
+            """
+            Compute the negative log-likelihood of an LNP model wih exponential non-linearity
+
+            :param wT: current receptive field array(stimDim[0]*stimDim[1],)
+            :param s: stimulus array(stimDim[0]*stimDim[1],T)
+            :param y: spiketimes array(,T)
+
+            :return nLL: computed negative log-likelihood scalar
+            :return dnLL: computed first derivative of the nLL
+            """
+
+            r = np.exp(np.dot(wT, s))
+            nLL = np.dot(r - y * np.log(r), np.ones(y.shape))
+
+            dnLL = np.dot(s * r - y * s, np.ones(y.shape))
+
+            return nLL, dnLL
+
+        def nLL_ridge(wT, s, y, theta):
+
+            """
+            Compute the negative log-likelihood of an LNP model wih exponential non-linearity
+
+            :param wT: current receptive field array(stimDim[0]*stimDim[1],)
+            :param s: stimulus array(stimDim[0]*stimDim[1],T)
+            :param y: spiketimes array(,T)
+
+            :return nLL: computed negative log-likelihood scalar
+            :return dnLL: computed first derivative of the nLL
+            """
+
+            r = np.exp(np.dot(wT, s))
+            nLL = np.dot(r - y * np.log(r), np.ones(y.shape)) + theta * np.dot(wT, wT)
+
+            dnLL = np.dot(s * r - y * s, np.ones(y.shape)) + 2 * theta * wT
+
+            return nLL, dnLL
+
+        # bin spiketimes as stimulus frames
+        T = s.shape[1]
+
+        y = np.histogram(spiketimes, bins=T,
+                         range=[triggertimes[0], triggertimes[len(triggertimes) - 1] + (fs / freq)])[0]
+        w0 = np.zeros(sta_inst.shape)
+
+        kf = KFold(T, n_folds=k)
+
+        theta_dict = {}
+        theta_dict['theta'] = theta
+        theta_dict['nLL train'] = []
+        theta_dict['nLL test'] = []
+        theta_dict['nLL mean test'] = []
+        theta_dict['pred perform'] = []
+        theta_dict['mean pred perform'] = []
+
+        for t in theta:
+
+            nLL_temp_train = []
+            nLL_temp_test = []
+            pred_test = []
+            for train, test in kf:
+                res = scipy.optimize.minimize(nLL_ridge, w0, args=(s[:, train], y[train], t), jac=True, method='TNC')
+                print(res.message, 'neg log-liklhd: ', res.fun)
+                nLL_temp_train.append(res.fun)
+                nLL_temp_test.append(nLL(res.x, s[:, test], y[test])[0])
+
+                y_test = np.zeros(len(test))
+                for t in range(len(test)):
+                    r = np.exp(np.dot(res.x, s[:, test[t]]))
+                    y_test[t] = np.random.poisson(lam=r)
+
+                pred_test.append((sum(y_test == y[test]) / len(test)))
+
+            theta_dict['pred perform'].append(pred_test)
+            theta_dict['mean pred perform'].append(np.mean(pred_test))
+            theta_dict['nLL train'].append(nLL_temp_train)
+            theta_dict['nLL test'].append(nLL_temp_test)
+            theta_dict['nLL mean test'].append(np.mean(nLL_temp_test))
+
+        theta_df = pd.DataFrame(theta_dict)
+
+        theta_opt = theta[np.where(theta_df['nLL mean test'] == min(theta_df['nLL mean test']))[0]]
+
+        LNP_dict = {}
+        LNP_dict.clear()
+        LNP_dict['nLL train'] = []
+        LNP_dict['nLL test'] = []
+        LNP_dict['w'] = []
+        LNP_dict['pred perform'] = []
+        LNP_dict['r2'] = []
+
+        for train, test in kf:
+            res = scipy.optimize.minimize(nLL_ridge, w0, args=(s[:, train], y[train], theta_opt), jac=True,
+                                          method='TNC')
+            print(res.message, 'neg log-liklhd: ', res.fun)
+
+            LNP_dict['nLL train'].append(res.fun)
+            LNP_dict['nLL test'].append(nLL(res.x, s[:, test], y[test])[0])
+            LNP_dict['w'].append(res.x)
+
+            y_test = np.zeros(len(test))
+            for t in range(len(test)):
+                r = np.exp(np.dot(res.x, s[:, test[t]]))
+                y_test[t] = np.random.poisson(lam=r)
+
+            LNP_dict['pred perform'].append((sum(y_test == y[test]) / len(test)))
+            LNP_dict['r2'].append(np.var(y[test]) / np.var(y_test))
+        LNP_df = pd.DataFrame(LNP_dict)
+
+        return LNP_df, theta_df
